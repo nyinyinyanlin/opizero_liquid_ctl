b133a14008dd45cb6b562919ac117d00b79e5cf6
nyinyinyanlin <nyinyinyanlin.mm@gmail.com>
2018-08-24 02:33:39 +0630

    'Cannot write to when a stream is already closed' error was caused by immediate exiting python script and it was caused by indentation mistake on main entry function call. Thus the script ended immediately and raised error to node.js parent process.

--- python/hcsr04.py
+++ python/hcsr04.py
@@ -39,5 +39,5 @@ def main():
                 print(distance)
                 sys.stdout.flush()
 
-                if __name__ == '__main__':
-                    main()
+if __name__ == '__main__':
+    main()
