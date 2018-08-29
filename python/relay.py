2775c927853267137d8489eb1f6c5617bb1e3df9
nyinyinyanlin <nyinyinyanlin.mm@gmail.com>
2018-08-29 17:39:00 +0630

    Added GPIO4 to connect to relay.

--- python/relay.py
+++ python/relay.py
@@ -5,13 +5,17 @@ from  pyGPIO.gpio import gpio, port
 def main():
 	gpio.init()
 	gpio.setcfg(port.STATUS_LED,1)
+	gpio.setcfg(port.gpio4,1)
 	gpio.output(port.STATUS_LED,0)
+	gpio.setcfg(port.gpio4,1)
 	print(sys.argv)
     	if("on"==sys.argv[1]):
         	gpio.output(port.STATUS_LED,1)
+	        gpio.output(port.gpio4,0)
         	print("STATUS_LED ON")
     	else:
         	gpio.output(port.STATUS_LED,0)
+			gpio.output(port.gpio4,1)
         	print("STATUS_LED OFF")
 
 if __name__ == '__main__':
